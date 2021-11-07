94df577f0f8511af1a7ba0fefbc656d03d6d39d1
Taoufik <malaouitaoufik1@gmail.com>
2021-11-08 00:54:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Malaoui Cooding</h1>
+      
+        <mymenu _></mymenu>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const mymenu_ = () => {
+    return (
+        <div>
+      <button type="button" class="btn btn-success">maaoui cooding</button>
+        </div>
+    )
+}
+
+export default mymenu_;
